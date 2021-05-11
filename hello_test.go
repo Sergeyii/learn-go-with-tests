623487cec85c764a41937529eb3d270af606dc04
Your Name <you@example.com>
2021-05-11 17:30:01 +0300

    Отрефакторен тест #2 "Hello, Chris" с использованием "подмножеств" в виде t.Run().

--- hello_test.go
+++ hello_test.go
@@ -2,23 +2,26 @@ package main
 
 import "testing"
 
-func TestHelloWorld(t *testing.T) {
-	//Ожидаю
-	expected := "Hello world!"
-
-	//Получилось на самом деле
-	got := HelloWorld()
-
-	if expected != got {
-		t.Errorf("Expected =%q, got=%q", expected, got)
-	}
-}
-
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		//Получил по факту
+		got := Hello("Chris")
+		//Ожидаю
+		want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got=%q, want=%q", got, want)
-	}
+		if got != want {
+			t.Errorf("got=%q, want=%q", got, want)
+		}
+	})
+
+	t.Run("saying 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
+		//Получил по факту
+		got := Hello("World")
+		//Ожидалось
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got=%q, want=%q", got, want)
+		}
+	})
 }
