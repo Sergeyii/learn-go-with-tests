8495472d6c9e60c94b346de187a725281fab4cc9
Your Name <you@example.com>
2021-05-18 14:09:27 +0300

    --Массивы и срезы(Arrays and slices)--
    --Чему мы научились:
    1. Срезы
    2. Вариативность путей их создания
    3. Как они имеют фиксированную ёмкость но вы можете создать новые срезы из старых используя append()
    4. Как делать срезы срезов!
    5. len() чтобы выяснить длину среза
    6. Инструмент покрытия тестов go test -cover
    7. reflect.DeepEqual и почему он полезен хотя может ухудшить типо-безопасность вашего кода

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -2,7 +2,7 @@ package arrays_slices
 
 import "fmt"
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		fmt.Println("number: ", number)
@@ -10,4 +10,27 @@ func Sum(numbers [5]int) int {
 	}
 
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
 }
