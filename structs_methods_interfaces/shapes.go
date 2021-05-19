e44feee5f245e483fa3f2bfe50d76553b4ea71b0
Your Name <you@example.com>
2021-05-19 09:17:56 +0300

    --Структуры, методы и интерфейсы--
    --Чему мы научились:
    1. Методы и интерфейсы

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,14 +1,28 @@
 package structs_methods_interfaces
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 func Perimeter(rect Rectangle) float64 {
 	return 2 * (rect.Width + rect.Height)
 }
 
-func Area(rect Rectangle) float64 {
-	return rect.Width * rect.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pow(c.Radius, 2) * math.Pi
 }
