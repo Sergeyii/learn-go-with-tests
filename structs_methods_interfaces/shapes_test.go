68cfde26e84a5d0862e399e7895b523d4c14d68d
Your Name <you@example.com>
2021-05-19 11:35:00 +0300

    --Структуры, методы и интерфейсы--
    --Чему мы научились:
    1. Табличные тесты

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -7,25 +7,33 @@ func TestPerimeter(t *testing.T) {
 	want := 40.0
 
 	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+		t.Errorf("got %.2f, hastArea %.2f", got, want)
 	}
 }
 
 func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name     string
+		shape    Shape
+		hastArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hastArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hastArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hastArea: 36.0},
+	}
+
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 
 		got := shape.Area()
 		if got != want {
-			t.Errorf("got=%.2f, want=%.2f", got, want)
+			t.Errorf("%#v, got=%.2f, want=%.2f", shape, got, want)
 		}
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		checkArea(t, Rectangle{12.0, 6.0}, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		checkArea(t, Circle{10}, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			checkArea(t, tt.shape, tt.hastArea)
+		})
+	}
 }
